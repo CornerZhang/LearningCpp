babcd5d44e47edbe95835b85d851d916709ffa05
CornerZhang <zhanghua@shinezone.com>
2015-08-31 12:46:39 +0800

    TCPL4 head version

--- InsideCpp11_code/InsideCpp11_code/main.cpp
+++ InsideCpp11_code/InsideCpp11_code/main.cpp
@@ -10,10 +10,17 @@
 #include "TestSTDC.h"
 #include "testUsingConstructor.h"
 
+int cmp( int a, int b )
+{
+    return (a<b) - (a>b);
+}
+
 int main(int argc, const char * argv[]) {
     TestEnumValue();
     TestSTDC_version();
     testUsingConstructor();
+    
+    int r = cmp(3, 8);
     
     return 0;
 }
---
+++ TCPL_4/TCPL_4/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  TCPL_4
+//
+//  Created by huazhang on 8/31/15.
+//  Copyright (c) 2015 huazhang. All rights reserved.
+//
+
+#include <iostream>
+
+using size_tl = unsigned int;
+
+int main(int argc, const char * argv[]) {
+    auto value = 0;
+    
+    return 0;
+}
